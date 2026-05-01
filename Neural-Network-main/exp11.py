55cf4b69147ff6b77dda1a2894adab3ff15518a7
Lavnish <lavnishstar111@gmail.com>
2026-05-01 13:45:06 +0530

    Update exp11.py

--- Neural-Network-main/exp11.py
+++ Neural-Network-main/exp11.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 print("===== HOPFIELD NETWORK EXPERIMENT =====")
 
-# -------------------------------
+# --------------------------------
 # 1. DEFINE PATTERNS
 # -------------------------------
 
